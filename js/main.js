5bdec9164fcadf1d50abd92d651fa10700afcf70
mehedihasan712277 <mehedihasan712277@gmail.com>
2022-02-12 16:58:33 +0600

    uploaded all files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// to show menubar
+function showLink() {
+    document.getElementById("id").style.width = "100%";
+}
+function hideLink() {
+    document.getElementById("id").style.width = "0px";
+}
